7f387bdabe85279812f047436bfbbe8700e5c81e
Hwiyun <Hwiyun>
2023-01-16 16:14:23 +0900

    servlet #2

--- helloweb/src/main/java/com/douzone/helloweb/controller/HelloServlet.java
+++ helloweb/src/main/java/com/douzone/helloweb/controller/HelloServlet.java
@@ -13,9 +13,12 @@ public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String id = request.getParameter("id");
+		
+		
 		response.setContentType("text/html; charset=utf-8");
 		PrintWriter pw = response.getWriter();
-		pw.print("<h1>Hello World</h1>");
+		pw.print("<h1>Hello~ " + (id == null ? "" : id) + "</h1>");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- helloweb/src/main/webapp/index.jsp
+++ helloweb/src/main/webapp/index.jsp
@@ -1,12 +1,16 @@
-<%@ page language="java" contentType="text/html; charset=EUC-KR"
-    pageEncoding="EUC-KR"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="EUC-KR">
+<meta charset="UTF-8">
 <title>Insert title here</title>
 </head>
 <body>
+	<% for(int i = 0; i < 5; i++) {%>
 	<h1>Hello World!!</h1>
+	<% } %>
+	
+	<a href="/helloweb/hello?id=kickscar">인사하기</a>
 </body>
 </html>
